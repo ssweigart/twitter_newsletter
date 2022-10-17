849d2668fa347cf4d0f842de6b5048b744d21b34
ssweigart <sarahcsweigart@gmail.com>
2022-10-17 13:11:47 -0700

    First code
    Here is the beta version of the jupiter notebook to pull and send the email. Further updates expected

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # twitter_newsletter
- code to email trending tweets from your area
+Sends you an email (daily eventually) with current tweets from your areas
+
+## Usage
+
+Pulls tweets from your area (either your profiles location or an established location) and sends you an email including topical trends and tweets from your areas (default: 5 topics, 5 tweets each)
---
+++ ps copy.py
@@ -0,0 +1,13 @@
+
+
+consumer_key="" #you have to get these off twitter
+consumer_secret="" #you have to get these off twitter
+
+access_token_secret="" #you have to get these off twitter
+access_token="" #you have to get these off twitter
+
+sender_email = "" #the email you are sending from. For security reasons, create a new account and don't use your personal account
+receiver_email = ""#where should the email go
+password_email = ""#password to the sender_email
+
+user_name = '' #your twitter handle
